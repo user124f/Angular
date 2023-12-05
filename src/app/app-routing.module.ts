af8c2e6e36690f18c5dabcc34931c59da2de916c
Rohan <rudraksh0322@gmail.com>
2023-12-05 14:09:36 +0530

    working good form created

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { Router, RouterModule, Routes } from '@angular/router';
+import { ListEmployeesComponent } from './employee/list-employees.component';
+import { CreateEmployeeComponent } from './employee/create-employee.component';
 
-const routes: Routes = [];
+const appRoutes: Routes = [
+  { path: 'list', component: ListEmployeesComponent },
+  { path: 'create', component: CreateEmployeeComponent },
+  { path: '', redirectTo: '/list', pathMatch: 'full' }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [
+    RouterModule,
+  RouterModule.forRoot(appRoutes)
+],
+exports:[RouterModule],
+declarations: []
 })
 export class AppRoutingModule { }
---
+++ src/app/employee/list-employees.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-list-employees',
+  templateUrl: './list-employees.component.html',
+  styleUrls: ['./list-employees.component.css']
+})
+export class ListEmployeesComponent {
+
+}

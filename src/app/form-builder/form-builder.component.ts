5e9d054e46a1a375252bb801877e8beb6a9d44f6
Rohan <rudraksh0322@gmail.com>
2023-12-20 14:09:47 +0530

    Move validation logic (Working Good)

--- src/app/form-builder/form-builder.component.ts
+++ src/app/form-builder/form-builder.component.ts
@@ -2,6 +2,19 @@ import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, FormControlName,FormBuilder, Validators} from '@angular/forms';
 
+
+interface IFormErrors {
+  [key: string]: string ;
+}
+
+interface IValidationMessages {
+  [key: string]: {[key: string]: string} ;
+}
+
+
+interface IMessages {
+  [key: string]: string ;
+}
 @Component({
   selector: 'app-create-employee',
   templateUrl: './form-builder.component.html',
@@ -10,37 +23,84 @@ import { FormGroup, FormControl, FormControlName,FormBuilder, Validators} from '
 export class FormBuilderComponent implements OnInit{
   employeeForm!: FormGroup;
 skillName: any;
+formErrors :IFormErrors = {
+  'fullName': '',
+  'email': '',
+  'skillName': '',
+  'experienceInYears': '',
+  'proficiency': ''
+};
+validationMessages:IValidationMessages = {
+  'fullName': {
+    'required': 'Full Name is required.',
+    'minlength': 'Full Name must be greater than 2 characters.',
+    'maxlength': 'Full Name must be less than 10 characters.'
+  },
+  'email': {
+    'required': 'Email is required.'
+  },
+  'skillName': {
+    'required': 'Skill Name is required.',
+  },
+  'experienceInYears': {
+    'required': 'Experience is required.',
+  },
+  'proficiency': {
+    'required': 'Proficiency is required.',
+  },
+};
+
 
  constructor(private fb: FormBuilder) {}
 
 
   ngOnInit() {
     this.employeeForm = this.fb.group({
-     fullName:['',[Validators.required,Validators.minLength(2),Validators.maxLength(10)]],
-     email:['',[Validators.required,Validators.email]],
-    skills: this.fb.group({
-      skillName:['',Validators.required,Validators.minLength(1)],
-      experienceInYears:['',Validators.required],
-      proficiency:['']
-     
-    }) 
-
+      fullName: ['', [Validators.required,
+      Validators.minLength(2), Validators.maxLength(10)]],
+      email: ['', Validators.required],
+      skills: this.fb.group({
+        skillName: ['', Validators.required],
+        experienceInYears: ['', Validators.required],
+        proficiency: ['', Validators.required]
     })
-  } 
+  });
 
-  onLoadDataClick(): void {
-    this.employeeForm.setValue({
-     fullName: 'Atharav',
-     email: 'aatharv123@gmail.com',
-     skills: {
-      skillName: 'Dance',
-      experienceInYears: '2',
-      proficiency: 'intermidiate'
+   this.employeeForm.valueChanges.subscribe((data) => {
+
+      this.logValidationErrors(this.employeeForm);
+
+   });
+   }
+
+  logValidationErrors(group: FormGroup = this.employeeForm): void {
+    Object.keys(group.controls).forEach((key: string) => {
+      const abstractControl = group.get(key);
+
+      if (abstractControl instanceof FormGroup) {
+        this.logValidationErrors(abstractControl);
+         } else {
+      
+          this.formErrors[key] = '';  
+      if (abstractControl && !abstractControl.valid && 
+        (abstractControl.touched || abstractControl.dirty)) {
+        const messages:IMessages = this.validationMessages[key] as IMessages;
+          
+        for (const errorKey in abstractControl.errors) {
+          if (errorKey) {
+            this.formErrors[key] += messages[errorKey] + ' ';
+            }
+          }
+      }
+    }
+  });
+   }
+ 
+
+   onLoadDataClick(): void {
+    /* this.logValidationErrors(this.employeeForm);
+    console.log(this.formErrors); */
      }
-
-    })
-
-  }
 
 onSubmit(): void {
   console.log(this.employeeForm.touched);
